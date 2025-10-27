99c0c119d32bf12365d23ca2f2b80e1e3cc6a0f3
L7-BUG <ll789y@gmail.com>
2025-10-27 11:15:04 +0800

    vue-demo-08:生命周期和模板引用

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,14 @@
 <script lang="ts" setup>
-import {computed, ref} from 'vue'
+import {onMounted, ref} from 'vue'
 
-interface Todo {
-  id: number,
-  text: string,
-  done: boolean,
-}
-
-const hideCompleted = ref<boolean>(true)
-
-let id: number = 0;
-const newTodo = ref<string>('')
-const todos = ref<Todo[]>([
-  {id: id++, text: 'Learn HTML', done: true},
-  {id: id++, text: 'Learn JavaScript', done: true},
-  {id: id++, text: 'Learn Vue', done: false}
-])
-
-const filteredTodos = computed<Todo[]>(() => {
-  if (hideCompleted.value) {
-    return todos.value;
-  } else {
-    return todos.value.filter(item => !item.done);
+const pElementRef = ref<HTMLElement | null>(null)
+onMounted(() => {
+  if (pElementRef.value !== null) {
+    pElementRef.value.textContent = 'mounted!';
   }
 })
-
-function addTodo() {
-  todos.value.push({id: id++, text: newTodo.value, done: false});
-}
-
-function removeTodo(todo: Todo) {
-  // ...
-  todos.value = todos.value.filter(item => item.id !== todo.id)
-}
 </script>
 
 <template>
-  <form @submit.prevent="addTodo">
-    <input v-model="newTodo" placeholder="new todo" required>
-    <button>Add Todo</button>
-  </form>
-  <ul>
-    <li v-for="todo in filteredTodos" :key="todo.id">
-      <input v-model="todo.done" type="checkbox">
-      {{ todo.text }}
-      <button @click="removeTodo(todo)">X</button>
-    </li>
-  </ul>
-  <button @click="hideCompleted = !hideCompleted">
-    {{ hideCompleted ? 'Show all' : 'Hide completed' }}
-  </button>
+  <p ref="pElementRef">Hello</p>
 </template>
-
-<style scoped>
-</style>
