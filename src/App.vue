44b42d4bbed1fd7c92954a0cebf84556857241c0
L7-BUG <ll789y@gmail.com>
2025-10-27 16:03:38 +0800

    测试类

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
 <script lang="ts" setup>
-import {ref} from "vue";
+import {reactive, ref} from "vue";
 
-const count = ref<number>(0);
+const count = ref(1);
+const h1Class = reactive({
+  red: count.value % 1 == 0,
+  green: count.value % 2 == 0,
+});
+const countAndDate = ref([
+  {count: count.value, date: Date.now()},
+])
+const temp = () => {
+  count.value++;
+  h1Class.red = count.value % 1 == 0;
+  h1Class.green = count.value % 2 == 0;
+  countAndDate.value.push({count: count.value, date: Date.now()});
+}
 </script>
 <template>
-  <button @click="count++">count++</button>
-  <h1>
-    {{ count }}
-  </h1>
+  <h1 :class="h1Class">App.vue</h1>
+  <button @click="temp">switch</button>
+  <h2>{{ count }}</h2>
+
+  <div v-for="item in countAndDate">
+    <h1>
+      count:{{ item.count }},添加时间:{{ item.date }}
+    </h1>
+  </div>
 </template>
+
+<style scoped>
+.red {
+  color: red;
+}
+
+.green {
+  color: green;
+}
+</style>
