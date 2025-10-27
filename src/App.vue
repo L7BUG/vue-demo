20e15869ad1251145fe76f842b98f0621db39622
L7-BUG <ll789y@gmail.com>
2025-10-27 11:49:05 +0800

    vue-demo-10:侦听器

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,35 @@
 <script lang="ts" setup>
-import {onMounted, ref} from 'vue'
+import {ref, watch} from 'vue'
 
-const pElementRef = ref<HTMLElement | null>(null)
-onMounted(() => {
-  if (pElementRef.value !== null) {
-    pElementRef.value.textContent = 'mounted!';
-  }
-})
+interface ResponseTodo {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean,
+}
+
+const todoId = ref(1)
+const todoData = ref<string>("")
+const responseTodo = ref<ResponseTodo | null>(null);
+
+async function fetchData() {
+  todoData.value = ""
+  const res = await fetch(
+      `https://jsonplaceholder.typicode.com/todos/${todoId.value}`
+  )
+  const data = await res.json()
+  console.log(data)
+  todoData.value = JSON.stringify(data)
+  responseTodo.value = data;
+}
+
+watch(todoId, fetchData)
+fetchData()
 </script>
 
 <template>
-  <p ref="pElementRef">Hello</p>
+  <p>Todo id: {{ todoId }}</p>
+  <button :disabled="!todoData" @click="todoId++">Fetch next todo</button>
+  <p v-if="!todoData">Loading...</p>
+  <pre v-else>{{ JSON.stringify(responseTodo) }}</pre>
 </template>
