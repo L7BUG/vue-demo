72ed1c14c7d08415ccaa2342cd76e10610d204ed
L7-BUG <ll789y@gmail.com>
2025-11-02 19:26:12 +0800

    拷贝登录页

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,12 @@ import { createPersistedState } from "pinia-plugin-persistedstate";
 const store = createPinia();
 store.use(createPersistedState());
 
-export * from "./modules/user";
-
 export { store };
+
+// export * from './modules/notification';
+// export * from './modules/permission';
+// export * from "./modules/setting";
+// export * from './modules/tabs-router';
+// export * from "./modules/user";
 
 export default store;
