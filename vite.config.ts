c38f6f34a1d0eded6ab5b3f015f57b1e55afe824
L7-BUG <ll789y@gmail.com>
2025-11-02 19:30:17 +0800

    拷贝登录页

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 
-import svgLoader from "vite-svg-loader";
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [vue(), svgLoader()],
+	plugins: [vue()],
 	resolve: {
 		alias: {
 			"@": resolve(__dirname, "src"),
